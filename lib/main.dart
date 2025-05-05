29c7ef484b450675d77b4b721aa18746bd63d865
Gernot <gernot.koller@gmail.com>
2025-05-05 13:49:11 +0200

    Umstyling main

--- lib/main.dart
+++ lib/main.dart
@@ -308,7 +308,42 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     if (_initializationError != null) {
       return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
+        //appBar: AppBar(title: Text(widget.title)),
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey.shade900,
+          elevation: 4,
+          foregroundColor: Colors.white,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 4,
+                    offset: Offset(1, 1),
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(4.0),
+              child: Image.asset(
+                'assets/logo/StoryTrail.png',
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          title: Text(
+            "StoryTrail",
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+
+
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(24.0),
@@ -364,6 +399,92 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 */
     return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey.shade900,
+        elevation: 4,
+        foregroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  offset: Offset(1, 1),
+                ),
+              ],
+            ),
+            padding: const EdgeInsets.all(4.0),
+            child: Image.asset(
+              'assets/logo/StoryTrail.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+
+
+/*
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(
+            'assets/logo/StoryTrail.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+*/
+        title: Text(
+          "StoryTrail",
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.bug_report),
+            tooltip: "Test Actions",
+            onPressed: () {
+              setState(() {
+                showActionTestingPanel = !showActionTestingPanel;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.sports_esports_outlined),
+            tooltip: "Simulate",
+            onPressed: () {
+              setState(() {
+                if (!_isSimulatingLocation) {
+                  _lastRealGpsPosition = _playerPosition;
+                } else {
+                  if (_lastRealGpsPosition != null) {
+                    _playerPosition = _lastRealGpsPosition!;
+                    _processNewLocation(_playerPosition);
+                  }
+                }
+                _isSimulatingLocation = !_isSimulatingLocation;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.menu_open),
+            tooltip: "Seitenleiste öffnen",
+            onPressed: () {
+              setState(() {
+                _isSidePanelVisible = !_isSidePanelVisible;
+              });
+            },
+          ),
+        ],
+      ),
+
+
+
+/*
       appBar: AppBar(
         title: Text(widget.title),
         actions: [
@@ -408,6 +529,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
+*/
       body:
           !_initializationCompleted
               ? Center(
